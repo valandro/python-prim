2d1d2e659f1b98fa83d0de3d268773353f6da722
valandro <lvalandrorocha@hotmail.com>
2018-12-10 09:14:27 -0200

    Parsing file

---
+++ src/prim.py
@@ -0,0 +1,24 @@
+from sys import argv
+import re
+
+file = open(argv[1], "r")
+# n = number of nodes
+n = 99
+# m = number of arrests
+m = 99
+graph = [[] * m] * n
+# [[{'2', '5'}],[{'1', '6'}]]
+# Each position of list is the node, and the structure is 'key' == node and 'value' == distance
+# Reading all file into a arrays of arrays
+for line in file:
+    # Reading only lines there are not comments
+    if not re.match("//", line):
+        info = line.split(" ")
+        # Remove \n of end of line
+        arrest = {info[1]: info[2].replace('\n', '')}
+        # If array is empty, so initalize them and append
+        if len(graph[int(info[0])]) == 0:
+            graph[int(info[0])] = []
+            graph[int(info[0])].append(arrest)
+        else:
+            graph[int(info[0])].append(arrest)
